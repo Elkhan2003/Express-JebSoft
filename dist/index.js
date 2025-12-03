d538019a609bd30fcaa800b0cec381d45186a7c5
Elcho911 <boss.armsport@gmail.com>
2025-12-03 20:33:04 +0600

    implement server setup with initial route handling

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Hello world!");
+const app_1 = require("./app");
+const server = (0, app_1.buildServer)();
+const start = async () => {
+    const PORT = 5000;
+    try {
+        server.listen({
+            port: PORT,
+            host: "0.0.0.0",
+        }, () => {
+            console.log(`${new Date()}`);
+            console.log(`server running at: http://localhost:${PORT}`);
+        });
+    }
+    catch (error) {
+        console.error("Error starting server:", error);
+    }
+};
+start();
 //# sourceMappingURL=index.js.map
