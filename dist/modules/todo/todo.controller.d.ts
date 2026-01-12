74e1bc9cdf17f9c271164a1fc64e0534616ce074
Elcho911 <boss.armsport@gmail.com>
2026-01-12 15:10:25 +0600

    refactor: update .gitignore, remove dist files, and enhance package.json and tsconfig.json

--- dist/modules/todo/todo.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from "express";
-declare const _default: {
-    getTodos: (req: Request, res: Response) => Promise<void>;
-};
-export default _default;
-//# sourceMappingURL=todo.controller.d.ts.map
