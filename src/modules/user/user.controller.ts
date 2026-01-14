1b6a0922cdaf42191cbedc56d4a4135d539eccbc
Elcho911 <boss.armsport@gmail.com>
2026-01-14 15:46:00 +0600

    feat: implement user CRUD operations and update routes

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { prisma } from "../../plugins/prisma";
 
 interface UserType {
 	name: string;
@@ -7,13 +8,7 @@ interface UserType {
 
 const getUsers = async (req: Request, res: Response) => {
 	try {
-		const data: UserType[] = [
-			{
-				name: "Санжар",
-				description: "Он мой друг",
-			},
-		];
-
+		const data = await prisma.user.findMany();
 		res.status(200).send({
 			success: true,
 			data: data,
@@ -23,4 +18,87 @@ const getUsers = async (req: Request, res: Response) => {
 	}
 };
 
-export default { getUsers };
+const getMe = async (req: Request, res: Response) => {
+	const { id } = req.params;
+	try {
+		const data = await prisma.user.findFirst({
+			where: {
+				id: Number(id),
+			},
+		});
+		res.status(200).send({
+			success: true,
+			data: data,
+		});
+	} catch (e) {
+		console.error("Error in getMe:", e);
+	}
+};
+
+const createUser = async (req: Request, res: Response) => {
+	try {
+		const { name, email } = req.body;
+		const data = await prisma.user.create({
+			data: {
+				name: name,
+				email: email,
+			},
+		});
+		res.status(201).send({
+			success: true,
+			result: data,
+		});
+	} catch (e) {
+		res.status(500).send({
+			success: false,
+			message: `Error creating user: ${e}`,
+		});
+	}
+};
+
+const updateUser = async (req: Request, res: Response) => {
+	const { id } = req.params;
+	const { name, email } = req.body;
+	try {
+		const data = await prisma.user.update({
+			where: {
+				id: Number(id),
+			},
+			data: {
+				name: name,
+				email: email,
+			},
+		});
+		res.status(200).send({
+			success: true,
+			result: data,
+		});
+	} catch (e) {
+		res.status(500).send({
+			success: false,
+			message: `Error updateUser: ${e}`,
+		});
+	}
+};
+
+const deleteUser = async (req: Request, res: Response) => {
+	try {
+		const { id } = req.params;
+		const data = await prisma.user.delete({
+			where: {
+				id: Number(id),
+			},
+		});
+		res.status(204).send({
+			success: true,
+			result: data,
+		});
+	} catch (e) {
+		res.status(500).send({
+			success: false,
+			message: `Error deleteUser: ${e}`,
+		});
+	}
+};
+
+export default { getUsers, getMe, createUser, updateUser, deleteUser };
--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -3,6 +3,10 @@ import userController from "./user.controller";
 
 const router = Router();
 
-router.get("/get-all", userController.getUsers);
+router.get("/get", userController.getUsers);
+router.get("/me/:id", userController.getMe);
+router.post("/create", userController.createUser);
+router.patch("/update/:id", userController.updateUser);
+router.delete("/delete/:id", userController.deleteUser);
 
 export default router;
